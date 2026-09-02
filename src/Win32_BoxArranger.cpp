1810fb58b03f701847b31d756e0c895c6cf8b2a2
Bogdan Mladin <bogdan.mladin12@gmail.com>
2026-09-02 15:07:08 +0200

    Window opens but does not close propperly

--- src/Win32_BoxArranger.cpp
+++ src/Win32_BoxArranger.cpp
@@ -1,26 +1,41 @@
 
 // clang-format off
 #include <windows.h>
+#include <errhandlingapi.h>
 #include <minwindef.h>
 #include <winuser.h>
 #include <debugapi.h>
 #include <cassert>
 // clang-format on
- 
+
 #include "BoxArranger.cpp"
 
 // open a window
 
-LRESULT CALLBACK WindowProcedure(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WindowProcedure(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam)
+{
     LRESULT result = 0;
 
-    switch (message){
-        default:
+    switch (message)
+    {
+    case WM_PAINT: {
+
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(windowHandle, &ps);
+
+        // All painting occurs here, between BeginPaint and EndPaint.
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(windowHandle, &ps);
+        break;
+    }
+
+    default:
         result = DefWindowProc(windowHandle, message, wParam, lParam);
     }
 
     return result;
-
 };
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
@@ -33,12 +48,12 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
     windowClass.hInstance = hInstance;
     windowClass.lpszClassName = "WindowClassName";
 
-    RegisterClass(&windowClass);
+    int hello = RegisterClass(&windowClass);
 
     HWND windowHandle = CreateWindowExA(0,
                                         windowClass.lpszClassName,
                                         "RenderThing",
-                                        WS_VISIBLE || WS_OVERLAPPED,
+                                        WS_VISIBLE | WS_OVERLAPPEDWINDOW,
                                         CW_USEDEFAULT,
                                         CW_USEDEFAULT,
                                         CW_USEDEFAULT,
@@ -47,20 +62,25 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
                                         0,
                                         hInstance,
                                         0);
+    if (windowHandle == 0)
+    {
+        assert(false);
+    }
 
     MSG message;
     BOOL getMessageReturn;
 
-    while ((getMessageReturn = GetMessage(&message, windowHandle, 0, 0)) != 0)
+    while ((getMessageReturn = GetMessage(&message, NULL, 0, 0)) != 0)
     {
         if (getMessageReturn == -1)
         {
-            assert(false);
+            int i = GetLastError();
+            return i;
         }
         else
         {
             TranslateMessage(&message);
-            DispatchMessage(&message);
+            DispatchMessage(&message); // this internally calls the correct window procedure
         }
     }
 };
